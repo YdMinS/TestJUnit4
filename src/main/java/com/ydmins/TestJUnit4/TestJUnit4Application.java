bf5daa38d6de676bea9ccf9e626ba11703543df8
ydmins <dr.ydmins@gmail.com>
2023-09-08 17:46:13 +0900

    first commit

---
+++ src/main/java/com/ydmins/TestJUnit4/TestJUnit4Application.java
@@ -0,0 +1,13 @@
+package com.ydmins.TestJUnit4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJUnit4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJUnit4Application.class, args);
+	}
+
+}
