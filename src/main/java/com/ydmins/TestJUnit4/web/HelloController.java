1442696c22337415a630eb27ac6cc6ea530aa86d
ydmins <dr.ydmins@gmail.com>
2023-09-10 19:26:12 +0900

    [ADD] HelloController, HelloControllerTest
    JUnit4를 이용한 테스트 코드 작성 및 실행해 보았다.

--- src/main/java/com/ydmins/TestJUnit4/web/HelloController.java
+++ src/main/java/com/ydmins/TestJUnit4/web/HelloController.java
@@ -1,2 +1,13 @@
-package com.ydmins.TestJUnit4.web;public class HelloController {
+package com.ydmins.TestJUnit4.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
